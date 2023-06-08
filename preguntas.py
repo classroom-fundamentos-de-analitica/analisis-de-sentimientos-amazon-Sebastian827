11d852326bc3782261865b5e1c8f8063ca74368d
unknown <seochoac@unal.edu.co>
2023-06-08 18:19:41 -0500

    prueba 2

--- preguntas.py
+++ preguntas.py
@@ -200,7 +200,7 @@ def pregunta_06():
 
     # pronostique la polaridad del sentimiento para los datos
     # no etiquetados
-    y_untagged_pred = gridSearchCV.predict()
+    y_untagged_pred = gridSearchCV.predict(X_untagged)
 
     # Retorne el vector de predicciones
     return y_untagged_pred
